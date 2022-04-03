e77565ee9ff3ee160aa05c1c41a546ffaf623adf
Samuel <sam200133@gmail.com>
2022-04-03 13:26:38 +0000

    Implemented ImageVectors

--- src/image/ImageVector.java
+++ src/image/ImageVector.java
@@ -1,6 +1,13 @@
 package image;
 
 import math.*;
+import java.awt.image.BufferedImage;
+import java.awt.color.ColorSpace;
+import java.awt.image.ColorConvertOp;
+import java.awt.image.Raster;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public class ImageVector extends Vector {
 	private final String fileName;
@@ -8,7 +15,83 @@ public class ImageVector extends Vector {
 	private final int height;
 	private final int width;
 
+	/**
+	 * Convert a image file into a Image vector where column are lined up one after
+	 * the other
+	 *
+	 * @param imagePath Path to the image to convert
+	 * @return an image vector
+	 */
+	private static ImageVector imageToVector(String imagePath) {
+		try {
+			// Read the image and place it in memory
+			BufferedImage imageBase = ImageIO.read(new File(imagePath));
+
+			// Convert colorspace to grayscale
+			ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+			ColorConvertOp op = new ColorConvertOp(cs, null);
+			BufferedImage image = op.filter(imageBase, null);
+
+			// Extract pixels data from the buffered image
+			Raster raster = image.getRaster();
+			System.out.println(raster.getNumBands());
+
+			// Create the which will contain the pixels data
+			double[] pixelVector = new double[image.getWidth() * image.getHeight()];
+
+			// Fill the vector
+			for (int x = 0; x < raster.getWidth(); x++) {
+				for (int y = 0; y < raster.getHeight(); y++) {
+
+					//
+					pixelVector[y + (x * raster.getHeight())] = raster.getSampleDouble(x, y, 0) / 255;
+				}
+			}
+
+			return new ImageVector(pixelVector, raster.getHeight(), raster.getWidth(), imagePath);
+
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	/**
+	 * Internal constructor to create a ImageVector from raw data
+	 *
+	 * @param vector    The vector of pixels
+	 * @param height    Height of the original image
+	 * @param width     Width of the original image
+	 * @param imagePath Path of the original image
+	 */
+	protected ImageVector(double[] vector, int height, int width, String imagePath) {
+		super(vector);
+		this.fileName = imagePath;
+		this.name = imagePath;
+		this.height = height;
+		this.width = width;
+	}
+
+	/**
+	 * Contructor by full copy
+	 *
+	 * @param imgV
+	 */
+	public ImageVector(ImageVector imgV) {
+		super(imgV);
+		this.fileName = imgV.getFileName();
+		this.name = imgV.getName();
+		this.height = imgV.getHeight();
+		this.width = imgV.getWidth();
+	}
+
+	/**
+	 * Constructor for ImageVactor using the path of the image to use
+	 *
+	 * @param imagePath Path to the og image
+	 */
 	public ImageVector(String imagePath) {
+		this(imageToVector(imagePath));
 	}
 
 	public String getFileName() {
@@ -25,5 +108,18 @@ public class ImageVector extends Vector {
 
 	public int getWidth() {
 		return (width);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("testImgVector");
+		ImageVector imgV = imageToVector("Benjamin_Blondey_0.jpg");
+		int index = 1;
+		for (double elements : imgV.getElements()) {
+			System.out.print(elements + "|");
+			if (index % 178 == 0) {
+				System.out.print("\n");
+			}
+			index++;
+		}
 	}
 }
--- src/math/Vector.java
+++ src/math/Vector.java
@@ -3,38 +3,46 @@ package math;
 public class Vector {
 	private double[] elements;
 
+	public Vector(Vector vector) {
+		this.elements = vector.getElements().clone();
+	}
+
 	public Vector(double[] elements) {
+		this.elements = elements.clone();
 	}
 
 	public int getLength() {
-		return(this.elements.length);
+		return (this.elements.length);
 	}
 
 	public double[] getElements() {
-		return(this.elements);
+		return (this.elements);
 	}
 
 	public static boolean equalLength(Vector v1, Vector v2) {
+		return true;
 	}
 
 	public static double distance(Vector v1, Vector v2) {
+		return 0.0;
 	}
 
 	public void multiply(double x) {
-		for (i = 0; i < elements.length; i++)
+		for (int i = 0; i < elements.length; i++)
 			elements[i] *= x;
 	}
 
 	public void add(Vector v) {
-		for (i = 0; i < elements.length; i++)
+		for (int i = 0; i < elements.length; i++)
 			elements[i] += v.getElements()[i];
 	}
 
 	public void subtract(Vector v) {
-		for (i = 0; i < elements.length; i++)
+		for (int i = 0; i < elements.length; i++)
 			elements[i] -= v.getElements()[i];
 	}
 
 	public double norm() {
+		return 0.0;
 	}
 }
