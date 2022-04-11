97a12033338fd0f4fd95799e59a02ad5e1b796e5
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-04-12 00:34:08 +0200

    args parser v1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import org.apache.commons.cli.*;
+
 public class Main {
 	public static void main(String[] args) {
+
+		Options options = new Options();
+
+		Option compile = Option.builder("c")
+				.desc("Compile database from images folder")
+				.longOpt("compile")
+				.hasArgs()
+				.argName("image folder path> <output file path")
+				.required()
+				.build();
+
+		Option test = Option.builder("t")
+				.desc("Test an image against database")
+				.longOpt("test")
+				.hasArgs()
+				.argName("image path> <compiled database file path")
+				.required()
+				.build();
+
+		options.addOption(compile);
+		options.addOption(test);
+
+		DefaultParser parser = new DefaultParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			HelpFormatter helper = new HelpFormatter();
+			helper.printHelp("java -jar <programme>", options);
+		}
+
 		//TODO argument parser
 		//Compiler
 		/* Argument formats
-		 * -c [/path/to/referencedb]?						(path defaults to ./img/reference if unspecified)
+		 * -c [/path/to/referencedb]?						Compiles the database in the specified directory (path defaults to ./img/reference if unspecified)
 		 * -c [/path/to/referencedb]? -k [value]			(k will need to be checked if it's higher than the number of images)
 		 */
 
