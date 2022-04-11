d05e0e5f8ca1cc095c71a7329c40e5eec6efe57d
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-04-12 00:50:33 +0200

    completed w/ todos

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,21 @@ public class Main {
 		Options options = new Options();
 
 		Option compile = Option.builder("c")
-				.desc("Compile database from images folder")
+				.desc("Compile database from images folder\n")
 				.longOpt("compile")
 				.hasArgs()
 				.argName("image folder path> <output file path")
 				.build();
 
 		Option test = Option.builder("t")
-				.desc("Test an image against database")
+				.desc("Test an image against database\n")
 				.longOpt("test")
 				.hasArgs()
 				.argName("image path> <compiled database file path")
 				.build();
 
 		Option k = Option.builder("k")
-				.desc("k value, optional")
+				.desc("k value, optional\n")
 				.hasArg()
 				.argName("value")
 				.required(false)
@@ -32,17 +32,22 @@ public class Main {
 
 		DefaultParser parser = new DefaultParser();
 		CommandLine cmd = null;
-		
+
 		try {
 			cmd = parser.parse(options, args);
 			if (cmd.hasOption("c")) {
 				String[] argsCompile = cmd.getOptionValues("c");
 				Compiler.compileDB(argsCompile[0], cmd.hasOption("k") ? Integer.parseInt(cmd.getOptionValue("k")) : 0);
+
+				// TODO : add option to save the database to a file
 			} else if (cmd.hasOption("t")) {
 				String[] argsTest = cmd.getOptionValues("t");
-				//if (argsTest.length == 2 && Compiler.verifyValidity(argsTest[1])) {
-				//	  Test.test(argsTest[0], argsTest[1]);
-				//}
+
+				// TODO : read database file
+				// TODO : test image against database
+				// if (argsTest.length == 2 && Compiler.verifyValidity(argsTest[1])) {
+				//	   Test.test(argsTest[0], argsTest[1]);
+				// }
 			} else {
 				throw new ParseException("No command specified");
 			}
@@ -50,17 +55,7 @@ public class Main {
 			HelpFormatter helper = new HelpFormatter();
 			helper.printHelp("java -jar <programme>", options);
 		}
-
-
-
-
-		//Tester
-		/* Argument formats
-		 * -t [/path/to/testimg]
-		 */
-
-		//call Compiler.verifyValidity() to verify if DB is good (up to date and output files not tampered with)
-		//then call Test.test(imagePath)
+		// TODO : catch everything else
 		
 	}
 }
