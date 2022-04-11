2cd2f2c60d4774eb385e460acbc3896be2f6e32f
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-04-12 00:44:42 +0200

    cli compile implementation

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
 				.longOpt("compile")
 				.hasArgs()
 				.argName("image folder path> <output file path")
-				.required()
 				.build();
 
 		Option test = Option.builder("t")
@@ -18,29 +17,41 @@ public class Main {
 				.longOpt("test")
 				.hasArgs()
 				.argName("image path> <compiled database file path")
-				.required()
+				.build();
+
+		Option k = Option.builder("k")
+				.desc("k value, optional")
+				.hasArg()
+				.argName("value")
+				.required(false)
 				.build();
 
 		options.addOption(compile);
+		options.addOption(k);
 		options.addOption(test);
 
 		DefaultParser parser = new DefaultParser();
 		CommandLine cmd = null;
+		
 		try {
 			cmd = parser.parse(options, args);
+			if (cmd.hasOption("c")) {
+				String[] argsCompile = cmd.getOptionValues("c");
+				Compiler.compileDB(argsCompile[0], cmd.hasOption("k") ? Integer.parseInt(cmd.getOptionValue("k")) : 0);
+			} else if (cmd.hasOption("t")) {
+				String[] argsTest = cmd.getOptionValues("t");
+				//if (argsTest.length == 2 && Compiler.verifyValidity(argsTest[1])) {
+				//	  Test.test(argsTest[0], argsTest[1]);
+				//}
+			} else {
+				throw new ParseException("No command specified");
+			}
 		} catch (ParseException e) {
 			HelpFormatter helper = new HelpFormatter();
 			helper.printHelp("java -jar <programme>", options);
 		}
 
-		//TODO argument parser
-		//Compiler
-		/* Argument formats
-		 * -c [/path/to/referencedb]?						Compiles the database in the specified directory (path defaults to ./img/reference if unspecified)
-		 * -c [/path/to/referencedb]? -k [value]			(k will need to be checked if it's higher than the number of images)
-		 */
 
-		//call Compiler.compileDB(dbPath, k)
 
 
 		//Tester
