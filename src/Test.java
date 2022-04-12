f1ad840243554302aef406a4c1b17426fa059a56
obito1903 <sam200133@gmail.com>
2022-04-12 23:30:37 +0200

    Implemented most of the test Class

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,61 @@
+import image.ImageVector;
+import math.*;
+
 public class Test {
-	public static void test(String imagePath) {
-		//TODO read matrices with Compiler.readCompiledMatrices()
-		//TODO read test image
-		//TODO multiply test image vector by G
-		//TODO compare result with every vector in E, return closest distance
+
+	/**
+	 * Calculate the distances between the test image and each images in the db
+	 *
+	 * @param db   The database to use
+	 * @param test The test image
+	 * @return An array of distances (doubles)
+	 */
+	public static double[] calculateDistances(EigenFacesDB db, ImageVector test) {
+		// Compute the projection of the test image
+		Vector testProjection = db.e.transpose().multiply(test);
+
+		// Compute the distance to each image
+		double[] distances = new double[db.g.getNbImages()];
+		for (int i = 0; i < db.g.getNbImages(); i++) {
+			distances[i] = Vector.distance(testProjection, db.g.getRow(i));
+		}
+
+		return distances;
+
+	}
+
+	/**
+	 * Find the closest image to the test image
+	 *
+	 * @param db   The database to use
+	 * @param test The test image
+	 * @return The name of the closest image
+	 */
+	public static String findBestMatch(EigenFacesDB db, ImageVector test) {
+
+		double[] distances = calculateDistances(db, test);
+
+		// Find the best match
+		double bestDistance = Double.MAX_VALUE;
+		String bestMatch = "";
+		for (int i = 0; i < db.g.getNbImages(); i++) {
+			if (distances[i] < bestDistance) {
+				bestDistance = distances[i];
+				bestMatch = db.g.getNameOf(i);
+			}
+		}
+
+		return bestMatch;
+	}
+
+	public static void main(String[] args) {
+		EigenFacesDB db = new EigenFacesDB("test.egdb");
+		// EigenFacesDB db = Compiler.compileDB("img/reference", 3);
+		ImageVector test = new ImageVector("img/test/Vincent_3.jpg");
+		double[] distances = calculateDistances(db, test);
+		for (int i = 0; i < distances.length; i++) {
+			System.out.println(db.g.getNameOf(i) + ": " + distances[i]);
+		}
+		System.out.println("Best match : " + findBestMatch(db, test));
 	}
 }
