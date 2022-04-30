bbf6224c445223352a2968c01b4874f6bee78f5c
baguettetrash <legrandale@cy-tech.fr>
2022-04-30 17:45:06 +0200

    JavaFx : configEGDB scene prototype based of sketch...

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,41 @@ import image.ImageSizeMismatchException;
 import image.ImageVector;
 import math.KValueOutOfBoundsException;
 
+import java.util.List;
+
 import org.apache.commons.cli.*;
 
 import javafx.application.Application;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.event.*;
-import javafx.geometry.Orientation;
 
-import java.io.File;
+import javafx.event.*;
+
+import javafx.geometry.Orientation;
+import javafx.geometry.Insets;
+
 import java.io.IOException;
 
 public class Main extends Application {
 
 	/*Class attributes of the different sources in the scene*/
-	//top left fileChooser
 	FileChooser egdbFileChooser = new FileChooser();
 	FileChooser outputFileChooser = new FileChooser();
-	//button associated to the fileChooser
-	Button egdbbutton = new Button("Select egdb");
-	Button outputbutton = new Button("Select output");
 
-	/*Create the egbd file chooser*/
 	public void createEgdbFileChooser(){
 		egdbFileChooser.setTitle("Open egdb file");
 		egdbFileChooser.getExtensionFilters().addAll(new ExtensionFilter("Egdb Files", "*.egdb"));
 	}
 
-	/*Create the output file chooser*/
 	public void createOutputFileChooser(){
 		egdbFileChooser.setTitle("Choose the final location");
 		//egdbFileChooser.getExtensionFilters().addAll(new ExtensionFilter("Egdb Files", "*.egdb"));
@@ -47,128 +47,60 @@ public class Main extends Application {
 
 		/*Title of the scene*/
 		primaryStage.setTitle("Facial Recognition");
+		
+		BorderPane windowEGDB = new BorderPane();
+		
+		VBox vb_loadEGDB = new VBox();
+		/*VBox formatting*/
+		//TODO
+		Button btn_loadEGDB = new Button("Load eigenfaces database");
+		btn_loadEGDB.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				/*Select EGDB folder*/
+				//TODO
+			}
+		});
+		vb.loadEGDB.getChildren().add(btn_loadEGDB);
 
-		/**/
-		VBox topLeft = new VBox();
-		BorderPane topRight = new BorderPane();
-		BorderPane bottomLeft = new BorderPane();
-		BorderPane bottomRight = new BorderPane();
+		VBox vb_eigenfaces = new VBox();
+		/*Table of all of the eigenfaces from the loaded database - ImageView*/
+		//TODO
 
-		/*Test*/
-		//topLeft.getChildren().add(new Label("1"));
-		topRight.setCenter(new Label("2"));
-        bottomLeft.setCenter(new Label("3"));
-        bottomRight.setCenter(new Label("4"));
+		HBox hb_compileEGDB = new HBox();
+		hb_compileEGDB.setPadding(new Insets(15,50,15,50));
+		hb_compileEGDB.setSpacing(20);
+		hb_compileEGDB.setStyle("-fx-background-color: #336699;"):
+		Button btn_compEGDB = new Button("Select EGDB file");
+		btn_compEGDB.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				/*Select EGDB file via filechooser*/
+				//TODO
+			}
+		});
+		Button btn_compOutput = new Button("Select Output folder");
+		btn_compOutput.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				/*Select Output folder*/
+				//TODO
+			}
+		});
+		Button btn_compile = new Button("Compile");
+		btn_compile.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				/*Feet compilation*/
+				//TODO
+			}
+		});
+		hb_compileEGDB.getChildren().addAll(btn_compEGDB, btn_compOutput, btn_compile);
 
-		/*Top Left*/
-		createEgdbFileChooser();
-		createOutputFileChooser();
-		//Create a label
-        Label label = new Label("no files selected");
-		//Create an Event Handler
-        EventHandler<ActionEvent> event =
-        new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                File file = egdbFileChooser.showOpenDialog(primaryStage);
-                if (file != null) {
-                    label.setText(file.getAbsolutePath() + "  selected");
-                }
-            }
-        };
-		//Setting up the file chooser button
-        egdbbutton.setOnAction(event);
-		topLeft.getChildren().add(egdbbutton);
-		//TO DO : output filechoose
+		windowEGDB.setTop(vb_loadEGDB);
+		windowEGDB.setCenter(vb_eigenfaces);
+		windowEGDB.setBottom(hb_compileEGDB);
 
-		/**/
-        TilePane root = new TilePane(Orientation.HORIZONTAL);
-		root.setPrefColumns(2);
-		root.setPrefTileWidth(640);
-		root.setPrefTileHeight(360);
+		Scene scene1 = new Scene(windowTest);
+		Scene scene2 = new Scene(windowEGDB);
 
-		/**/
-		root.getChildren().addAll(topLeft, topRight, bottomLeft, bottomRight);
-
-		/**/
-		Scene scene = new Scene(root, 1280, 720);
-		primaryStage.setScene(scene);
+		primaryStage.setScene(scene2, 1280, 720);
 		primaryStage.show();
 	}
-
-	public static void main(String[] args) {
-
-		launch(args);
-
-		/*Options options = new Options();
-
-		Option compile = Option.builder("c")
-				.desc("Compile database from images folder\n")
-				.longOpt("compile")
-				.hasArgs()
-				.argName("image folder path> <output file path")
-				.build();
-
-		Option test = Option.builder("t")
-				.desc("Test an image against database\n")
-				.longOpt("test")
-				.hasArgs()
-				.argName("image path> <compiled database file path")
-				.build();
-
-		Option k = Option.builder("k")
-				.desc("k value, optional\n")
-				.hasArg()
-				.argName("value")
-				.required(false)
-				.build();
-
-		Option debug = Option.builder("d")
-				.desc("Debug mode, optional\n")
-				.longOpt("debug")
-				.hasArg()
-				.argName("output folder")
-				.required(false)
-				.build();
-
-		options.addOption(compile);
-		options.addOption(k);
-		options.addOption(test);
-		options.addOption(debug);
-
-		DefaultParser parser = new DefaultParser();
-		CommandLine cmd = null;
-
-		try {
-			cmd = parser.parse(options, args);
-			if (cmd.hasOption("c") && cmd.getOptionValues("c").length == 2) {
-				String[] argsCompile = cmd.getOptionValues("c");
-				EigenFacesDB db = Compiler.compileDB(argsCompile[0], cmd.hasOption("k") ? Integer.parseInt(cmd.getOptionValue("k")) : 0, cmd.hasOption("d") ? cmd.getOptionValue("d") : null);
-				db.saveToFile(argsCompile[1]);
-
-
-			} else if (cmd.hasOption("t") && cmd.getOptionValues("t").length == 2) {
-				String[] argsTest = cmd.getOptionValues("t");
-
-				EigenFacesDB db = new EigenFacesDB(argsTest[1]);
-
-				System.out.println("Best match against database : " + Test.findBestMatch(db, new ImageVector(argsTest[0]), cmd.hasOption("d")));
-				
-			} else {
-				throw new ParseException("No command specified");
-			}
-		} catch (ParseException e) {
-			HelpFormatter helper = new HelpFormatter();
-			helper.printHelp("java -jar <programme>", options);
-		} catch (IOException e) {
-			System.out.println("Error while reading file : " + e.getMessage());
-		} catch (ClassNotFoundException e) {
-			System.out.println("Error while loading database file, trying to load class.");
-		} catch (KValueOutOfBoundsException e) {
-			System.out.println("Error while loading database file : " + e.getMessage());
-		} catch (ImageSizeMismatchException e) {
-			System.out.println("Error while loading image : " + e.getMessage());
-		}*/
-		
-	}
-}
+}	
