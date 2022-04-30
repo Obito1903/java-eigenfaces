a9100e22c1178ef488f1acab6029d541f7d55716
baguettetrash <legrandale@cy-tech.fr>
2022-04-30 18:29:07 +0200

    JavaFx: update on the configEGDB scene : free of bugs but needs eventhandlers and eigenfaces table display

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
@@ -47,33 +48,48 @@ public class Main extends Application {
 
 		/*Title of the scene*/
 		primaryStage.setTitle("Facial Recognition");
-		
-		BorderPane windowEGDB = new BorderPane();
-		
-		VBox vb_loadEGDB = new VBox();
-		/*VBox formatting*/
-		//TODO
+	/*Scene1*/
+		BorderPane recognitionTest = new BorderPane();
+	/*Scene2*/
+		VBox configEGDB = new VBox();
+	/*Return button*/	
+		HBox hb_back = new HBox();
+		hb_back.setPadding(new Insets(15,0,15,25));
+		Button btn_back = new Button("Back");
+		btn_back.setPrefSize(70,60);
+		btn_back.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				/*Swap back to the 'test' scene*/
+				//TODO (perhaps use windows instead of scene changes - to be discussed)
+			}
+		});
+		hb_back.getChildren().add(btn_back);
+	/*EGDB loading button*/
+		HBox hb_loadEGDB = new HBox();
+		hb_loadEGDB.setPadding(new Insets(20,0,20,410));
 		Button btn_loadEGDB = new Button("Load eigenfaces database");
+		btn_loadEGDB.setPrefSize(250,30);
 		btn_loadEGDB.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				/*Select EGDB folder*/
 				//TODO
 			}
 		});
-		vb.loadEGDB.getChildren().add(btn_loadEGDB);
-
+		hb_loadEGDB.getChildren().add(btn_loadEGDB);
+	/*EGDB table*/
 		VBox vb_eigenfaces = new VBox();
+		vb_eigenfaces.setPadding(new Insets(50,200,500,200));
 		/*Table of all of the eigenfaces from the loaded database - ImageView*/
 		//TODO
-
+	/*EGDB compilation menu*/
 		HBox hb_compileEGDB = new HBox();
-		hb_compileEGDB.setPadding(new Insets(15,50,15,50));
+		hb_compileEGDB.setPadding(new Insets(15,50,15,320));
 		hb_compileEGDB.setSpacing(20);
-		hb_compileEGDB.setStyle("-fx-background-color: #336699;"):
-		Button btn_compEGDB = new Button("Select EGDB file");
-		btn_compEGDB.setOnAction(new EventHandler<ActionEvent>() {
+		hb_compileEGDB.setStyle("-fx-background-color: #336699;");
+		Button btn_compRef = new Button("Select reference folder");
+		btn_compRef.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				/*Select EGDB file via filechooser*/
+				/*Select image references folder*/
 				//TODO
 			}
 		});
@@ -88,19 +104,17 @@ public class Main extends Application {
 		btn_compile.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				/*Feet compilation*/
-				//TODO
+				//TODO (not selectable unless folders chosen)
 			}
 		});
-		hb_compileEGDB.getChildren().addAll(btn_compEGDB, btn_compOutput, btn_compile);
+		hb_compileEGDB.getChildren().addAll(btn_compRef, btn_compOutput, btn_compile);
 
-		windowEGDB.setTop(vb_loadEGDB);
-		windowEGDB.setCenter(vb_eigenfaces);
-		windowEGDB.setBottom(hb_compileEGDB);
+		configEGDB.getChildren().addAll(hb_back, hb_loadEGDB, vb_eigenfaces, hb_compileEGDB);
 
-		Scene scene1 = new Scene(windowTest);
-		Scene scene2 = new Scene(windowEGDB);
+		Scene scene1 = new Scene(recognitionTest, 1080, 720);
+		Scene scene2 = new Scene(configEGDB, 1080, 720);
 
-		primaryStage.setScene(scene2, 1280, 720);
+		primaryStage.setScene(scene2);
 		primaryStage.show();
 	}
 }	
